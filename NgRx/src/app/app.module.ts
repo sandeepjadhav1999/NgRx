ea1652d54807854c752930b1a8d512d25e5d7da6
sandeep jadhav <sandeep.jadhav2409@gmail.com>
2023-07-10 11:25:26 +0530

    initial setup

---
+++ NgRx/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
+import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
+import { CounterComponent } from './counter/counter/counter.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CounterOutputComponent,
+    CounterButtonsComponent,
+    CounterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ NgRx/src/app/counter/counter-buttons/counter-buttons.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-buttons',
+  templateUrl: './counter-buttons.component.html',
+  styleUrls: ['./counter-buttons.component.css']
+})
+export class CounterButtonsComponent {
+
+}
